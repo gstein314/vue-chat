f0d5fd6b02ee5d9e1854fc2d58e21d76260cbead
Hikaru Gyoji <49551505+gstein314@users.noreply.github.com>
2022-08-15 00:32:51 +0900

    change variables name

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,12 +6,12 @@ import firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "${{ secrets.API_KEY }}",
-  authDomain: "${{ secrets.AUTH_DOMAIN }}",
-  projectId: "${{ secrets.PROJECT_ID }}",
-  storageBucket: "${{ secrets.STORAGE_BUCKET }}",
-  messagingSenderId: "${{ secrets.MESSAGING_SENDER_ID }}",
-  appId: "${{ secrets.APP_ID }}"
+  apiKey: process.env.VUE_API_KEY,
+  authDomain: process.env.VUE_AUTH_DOMAIN,
+  projectId: process.env.VUE_PROJECT_ID,
+  storageBucket: process.env.VUE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_ID
 };
 
 // Initialize Firebase
