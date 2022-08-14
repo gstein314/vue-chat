08852af97457aadde64206bf8c301772c3a0c6be
gstein314 <gstein314@users.noreply.github.com>
2022-08-14 23:51:40 +0000

    deploy: 228f73215caecafc25a90a1c19ae9f3434d4485b

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ],
+  publicPath: isProd ? '/vue-chat' : '/',
+  outputDir: 'dist',
+  filenameHashing: false,
+  productionSourceMap: false
+})
