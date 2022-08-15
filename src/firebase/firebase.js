f11498b9d18e5d1b74ed0b1a69da81595447ee9f
行事光 <h.gyoji.vrds@gmail.com>
2022-08-16 00:30:26 +0900

    fix keys notation

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,12 +5,12 @@ import firebase from 'firebase';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: process.env.VUE_API_KEY,
-  authDomain: process.env.VUE_AUTH_DOMAIN,
-  projectId: process.env.VUE_PROJECT_ID,
-  storageBucket: process.env.VUE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_MESSAGING_SENDER_ID,
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_ID
 };
 
