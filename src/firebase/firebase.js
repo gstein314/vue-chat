5747f8a16ace3e50755b6111755c37b95ebe63b2
行事光 <h.gyoji.vrds@gmail.com>
2022-08-16 00:53:40 +0900

    add databaseUrl

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,7 +11,8 @@ const firebaseConfig = {
   projectId: process.env.VUE_APP_PROJECT_ID,
   storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_ID
+  appId: process.env.VUE_APP_ID,
+  databaseURL: process.env.VUE_APP_DATABASE_URL
 };
 
 // Initialize Firebase
